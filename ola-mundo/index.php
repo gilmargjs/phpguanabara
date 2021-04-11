0a9179fb17f0b720fb3d0f5fb7e1f53fdc39365f
Gilmar Jose <gilmarjose2014@gmail.com>
2021-04-11 17:26:03 -0300

    ola mundo,  variaveis

---
+++ ola-mundo/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo em Php</title>
+</head>
+<body>
+    
+    <h1>Testando Php</h1>
+    <?Php
+    echo "olá mundo!";
+    ?>
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo em Php</title>
+</head>
+<body>
+    
+    <h1>Testando Php</h1>
+    <?Php
+    $nome1 = "Jois";
+    $idade1 = 39;
+    $nome2 = "Gilmar";
+    $idade2 = 42;
+    $fone1 = "(81) 973309965";
+    $fone2 = "(81) 998921541";
+    //echo $nome1." é o nome da mulher " .$nome2." é o nome do marido ".","." o telefone de ".$nome1." é " .$fone1. "e o telefone de ".$nome2." é ".$fone2;
+    echo " $nome1 é o nome da mulher $nome2 é o nome do marido o telefone de $nome1 é $fone1 e o telefone de $nome2 é $fone2 ";
+    ?>
+</body>
+</html>
